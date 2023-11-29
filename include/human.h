cd2ed6bd579b2f30a061d86578ea9438b0cd87d7
OxiaozhaiO <52xiaozhai@gmail.com>
2023-11-29 14:20:54 +0800

    移除函数的实现

--- include/human.h
+++ include/human.h
@@ -6,18 +6,12 @@ class human
 private:
 	string name;
 	int age, num;
+	string stat;
 public:
-	void talkAbout(string selfname)
-	{
-		cout<<getName()<<":\n\t"<<"hi~ "<<selfname<<endl;
-	}
-	human()
-	{
-		name = "SYW";
-		age = 16;
-		num = 50;
-	}
-	string getName(){return name;}
-	int getAge(){return age;}
-    int getNum(){return num;}
+	human();
+	void talkAbout(string selfname);
+	string getName();
+	int getAge();
+    int getNum();
+	void setNum(int num);
 };
